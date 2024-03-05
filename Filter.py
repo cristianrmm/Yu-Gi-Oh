25c56f81439ce52a2b5729e0bb7498c7c395c4c9
cristianrmm <cristianmoncada@ymail.com>
2024-03-04 23:51:08 -0600

    Update Filter.py
    generated a basic filter class that categorize cards based on archetype, frame, race, and level. further work is needed in order to put multiple restrictions into a single search.

--- Filter.py
+++ Filter.py
@@ -10,12 +10,66 @@ class Filter():
             set.append(i[0])
         return set
 
+    def RaceType(self):
+        frame = self.mydb.cursor()
+        frame.execute("SELECT race FROM cards WHERE frameType != 'skill' ORDER BY race")
+        set = ['all']
+        for i in self.UniqueSet(frame.fetchall()):
+            set.append(i[0])
+        return set
+
+    def Level(self):
+        frame = self.mydb.cursor()
+        frame.execute("SELECT level FROM cards WHERE level > '0' ORDER BY CAST(level AS SIGNED INTEGER)")
+        set = ['all']
+        for i in self.UniqueSet(frame.fetchall()):
+            set.append(i[0])
+        return set
+
+
+    def ArcheType(self):
+        frame = self.mydb.cursor()
+        frame.execute("SELECT archetype FROM cards ORDER BY archetype")
+        set = ['all']
+        for i in self.UniqueSet(frame.fetchall()):
+            if i[0] == "":
+                set.append('undefined')
+            else:
+                set.append(i[0])
+        return set
+
     def getAllFrameCards(self, frame):
         card = self.mydb.cursor()
         if frame == 'all':
             card.execute("SELECT id, name, description, frameType, card_index FROM cards")
         else:
             card.execute("SELECT id, name, description, frametype, card_index FROM cards WHERE frameType LIKE '" + frame +"'")
+        return card.fetchall()
+
+    def getAllRaceCards(self, race):
+        card = self.mydb.cursor()
+        if race == 'all':
+            card.execute("SELECT id, name, description, frameType, card_index FROM cards")
+        else:
+            card.execute("SELECT id, name, description, frametype, card_index FROM cards WHERE race LIKE '" + race +"'")
+        return card.fetchall()
+
+    def getAllLevelCards(self, level):
+        card = self.mydb.cursor()
+        if level == 'all':
+            card.execute("SELECT id, name, description, frameType, card_index FROM cards")
+        else:
+            card.execute("SELECT id, name, description, frametype, card_index FROM cards WHERE level LIKE '" + level +"'")
+        return card.fetchall()
+
+    def getAllArchCards(self, archetype):
+        card = self.mydb.cursor()
+        if archetype == 'all':
+            card.execute("SELECT id, name, description, frameType, card_index FROM cards")
+        elif (archetype == 'undefined'):
+            card.execute("SELECT id, name, description, frametype, card_index FROM cards WHERE archetype LIKE ''")
+        else:
+            card.execute("SELECT id, name, description, frametype, card_index FROM cards WHERE archetype LIKE (%s)",  (archetype,))
         return card.fetchall()
 
     def UniqueSet(self, cardsInfo):
