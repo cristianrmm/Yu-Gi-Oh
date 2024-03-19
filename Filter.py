ee31192a5368a840f81abee5b8c147d753441886
cristianrmm <cristianmoncada@ymail.com>
2024-03-18 23:58:52 -0500

    Update Filter.py
    now the if the user tries to select an invalid specification it will revert back to the previous selection. just need to add the defense of the monster to consider it complete (for now).

--- Filter.py
+++ Filter.py
@@ -1,11 +1,60 @@
 class Filter():
     def __init__(self, myDB):
+        self.indexFrame = 0
+        self.indexArche = 0
+        self.indexRace = 0
+        self.indexLevel = 0
+        self.indexAttribute = 0
+        self.indexAttack = 0
         self.frame = 'all'
-        self.archType = 'all'
+        self.archeType = 'all'
         self.race = 'all'
         self.level = 'all'
-        self.getMyCards = ['all', 'all', 'all', 'all']
+        self.attribute = 'all'
+        self.attack = 'all'
+        self.getMyCards = ['all', 'all', 'all', 'all', 'all', 'all']
+        self.getMyPreviousCards = ['all', 'all', 'all', 'all', 'all', 'all']
         self.mydb = myDB
+
+    def SetPrevious(self, type,  index, frameType):
+        if (type == 'frame'):
+            self.getMyPreviousCards[0] = frameType
+            self.indexFrame = index
+        elif (type == 'arche'):
+            self.getMyPreviousCards[1] = frameType
+            self.indexArche = index
+        elif (type == 'race'):
+            self.getMyPreviousCards[2] = frameType
+            self.indexRace = index
+        elif (type == 'level'):
+            self.getMyPreviousCards[3] = frameType
+            self.indexLevel = index
+        elif (type == 'attribute'):
+            self.getMyPreviousCards[4] = frameType
+            self.indexAttribute = index
+        elif (type == 'attack'):
+            self.getMyPreviousCards[5] = frameType
+            self.indexAttack = index
+
+    def GetPrevious(self, type, frameType):
+        if (type == 'frame'):
+            self.frame = self.getMyPreviousCards[0]
+            return self.indexFrame
+        elif (type == 'arche'):
+            self.archeType = self.getMyPreviousCards[1]
+            return self.indexArche
+        elif (type == 'race'):
+            self.race = self.getMyPreviousCards[2]
+            return self.indexRace
+        elif (type == 'level'):
+            self.level = self.getMyPreviousCards[3]
+            return self.indexLevel
+        elif (type == 'attribute'):
+            self.attribute = self.getMyPreviousCards[4]
+            return self.indexAttribute
+        elif (type == 'attack'):
+            self.attack = self.getMyPreviousCards[4]
+            return self.indexAttack
 
     def FrameType(self):
         cardFrame = self.mydb.cursor()
@@ -31,6 +80,17 @@ class Filter():
             set.append(i[0])
         return set
 
+    def Attribute(self):
+        cardFrame =  self.mydb.cursor()
+        cardFrame.execute("SELECT attribute FROM cards")
+        set = ['all']
+        for i in self.UniqueSet(cardFrame.fetchall()):
+            if i[0] == "":
+                set.append('undefined')
+            else:
+                set.append(i[0])
+        return set
+
     def ArcheType(self):
         cardFrame = self.mydb.cursor()
         cardFrame.execute("SELECT archetype FROM cards ORDER BY archetype")
@@ -42,17 +102,27 @@ class Filter():
                 set.append(i[0])
         return set
 
-    def GetFrame(self, frame):
-        self.frame = frame
+    def Attack(self):
+        cardFrame = self.mydb.cursor()
+        cardFrame.execute("SELECT atk FROM cards WHERE atk != '' ORDER BY CAST(atk AS SIGNED INTEGER)")
+        set = ['all']
+        for i in self.UniqueSet(cardFrame.fetchall()):
+            set.append(i[0])
+        return set
 
-    def GetArchType(self, archType):
-        self.archType = archType
-
-    def GetRace(self, race):
-        self.race = race
-
-    def GetLevel(self, level):
-        self.level = level
+    def GetCardInfo(self, cardType, info):
+        if cardType == 'frame':
+            self.frame = info
+        elif cardType == 'arche':
+            self.archeType = info
+        elif cardType == 'race':
+            self.race = info
+        elif cardType == 'level':
+            self.level = info
+        elif cardType == 'attribute':
+            self.attribute = info
+        elif cardType == 'attack':
+            self.attack = info
 
     def GetAllCards(self):
         card = self.mydb.cursor()
@@ -60,29 +130,46 @@ class Filter():
         archType = ''
         race = ''
         level = ''
+        attribute = ''
+        attack = ''
         myset = ''
 
         set = []
         if (self.frame != 'all'):
             frame = "frameType LIKE '" + self.frame + "'"
             set.append(frame)
-        if (self.archType != 'all' and self.archType != 'undefined'):
+
+        if (self.archeType != 'all' and self.archeType != 'undefined'):
             archType = "archeType LIKE (%s)"
             set.append(archType)
-        elif(self.archType == 'undefined'):
+        elif(self.archeType == 'undefined'):
             archType = "archeType LIKE ''"
             set.append(archType)
+
         if (self.race != 'all'):
             race = "race LIKE '" + self.race + "'"
             set.append(race)
+
         if (self.level != 'all'):
             level = "level LIKE '" + self.level + "'"
             set.append(level)
+
+        if (self.attack != 'all'):
+            attack = "atk LIKE '" + self.attack + "'"
+            set.append(attack)
+
+
+        if (self.attribute != 'all' and self.attribute != 'undefined'):
+            attribute = "attribute LIKE '" + self.attribute  + "'"
+        elif (self.attribute == 'undefined'):
+            attribute = "attribute LIKE ''"
 
         self.getMyCards[0] = frame
         self.getMyCards[1] = archType
         self.getMyCards[2] = race
         self.getMyCards[3] = level
+        self.getMyCards[4] = attribute
+        self.getMyCards[5] = attack
 
         if len(self.getMyCards) > 1:
             n = 0
@@ -93,14 +180,14 @@ class Filter():
                 elif len(i) != 0 and n != 0:
                     myset = myset + " AND " + str(i)
 
-        if (self.frame == 'all' and self.archType == 'all' and self.race == 'all' and self.level == 'all'):
+        if (self.frame == 'all' and self.archeType == 'all' and self.race == 'all' and self.level == 'all' and self.attribute == 'all' and attack == 'all'):
             card.execute("SELECT id, name, description, frameType, card_index FROM cards")
-        elif (self.archType == 'all'):
+        elif (self.archeType == 'all'):
             card.execute("SELECT id, name, description, frameType, card_index FROM cards WHERE " + myset)
-        elif (self.archType == 'undefined'):
+        elif (self.archeType == 'undefined'):
             card.execute("SELECT id, name, description, frameType, card_index FROM cards WHERE " + myset)
         else:
-            card.execute("SELECT id, name, description, frameType, card_index FROM cards WHERE " + myset, (self.archType,))
+            card.execute("SELECT id, name, description, frameType, card_index FROM cards WHERE " + myset, (self.archeType,))
 
         return card.fetchall()
 
