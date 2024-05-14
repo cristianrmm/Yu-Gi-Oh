c070563538a349f5660c938075bb66e15e447224
cristianrmm <cristianmoncada@ymail.com>
2024-05-14 00:07:31 -0500

    Update Field.py
    i moved the field form the main class to this class for simplification. I subdivided the two players into their own variables in order to maintain their own decks. here is where i added digital numbers for the life points for both of the players

--- Field.py
+++ Field.py
@@ -8,12 +8,13 @@ class Field():
         self.myDB = myDB
         self.screen = screen
         self.RATIO = 397/271
-        self.deck = []
         self.width = 0
         self.height = 0
 
         self.phase = ['Draw Phace', 'Standby Phase', 'Main Phase1', 'Battale Phase', 'Main Phase2', 'End Phase']
+
         self.id = {}
+        self.deck = []
         self.hand = []
         self.monsterzone = []
         self.spelltrapzone = []
@@ -21,6 +22,57 @@ class Field():
         self.outOfPlay = []
         self.mainDeck = []
         self.extraDeck = []
+        self.lifePoints = 8000
+
+        self.opId = {}
+        self.opDeck = []
+        self.opHand = []
+        self.opMonsterzone = []
+        self.opSpelltrapzone = []
+        self.opGraveYeard = []
+        self.opOutOfPlay = []
+        self.opMainDeck = []
+        self.opExtraDeck = []
+        self.opLifePoints = 8000
+
+    def Field(self, x, y, w, h):
+        # self.DrawBox(400 + (self.GetHeight() + 30) * n, 20 + (self.GetHeight() + 10) * m)
+        self.DrawBox(x + (self.GetHeight() + w) * -1, y + (self.GetHeight() + h) * 0)
+        self.DrawBox(x + (self.GetHeight() + w) * 0, y + (self.GetHeight() + h) * 0)
+        self.DrawBox(x + (self.GetHeight() + w) * 1, y + (self.GetHeight() + h) * 0)
+        self.DrawBox(x + (self.GetHeight() + w) * 2, y + (self.GetHeight() + h) * 0)
+        self.DrawBox(x + (self.GetHeight() + w) * 3, y + (self.GetHeight() + h) * 0)
+        self.DrawBox(x + (self.GetHeight() + w) * 4, y + (self.GetHeight() + h) * 0)
+        self.DrawBox(x + (self.GetHeight() + w) * 5, y + (self.GetHeight() + h) * 0)
+
+        self.DrawBox(x + (self.GetHeight() + w) * -2, y + (self.GetHeight() + h) * 1)
+        self.DrawBox(x + (self.GetHeight() + w) * -1, y + (self.GetHeight() + h) * 1)
+        self.DrawBox(x + (self.GetHeight() + w) * 0, y + (self.GetHeight() + h) * 1)
+        self.DrawBox(x + (self.GetHeight() + w) * 1, y + (self.GetHeight() + h) * 1)
+        self.DrawBox(x + (self.GetHeight() + w) * 2, y + (self.GetHeight() + h) * 1)
+        self.DrawBox(x + (self.GetHeight() + w) * 3, y + (self.GetHeight() + h) * 1)
+        self.DrawBox(x + (self.GetHeight() + w) * 4, y + (self.GetHeight() + h) * 1)
+        self.DrawBox(x + (self.GetHeight() + w) * 5, y + (self.GetHeight() + h) * 1)
+
+        self.DrawBox(x + (self.GetHeight() + w) * 1, y + (self.GetHeight() + h) * 2)
+        self.DrawBox(x + (self.GetHeight() + w) * 3, y + (self.GetHeight() + h) * 2)
+
+        self.DrawBox(x + (self.GetHeight() + w) * -1, y + (self.GetHeight() + h) * 3)
+        self.DrawBox(x + (self.GetHeight() + w) * 0, y + (self.GetHeight() + h) * 3)
+        self.DrawBox(x + (self.GetHeight() + w) * 1, y + (self.GetHeight() + h) * 3)
+        self.DrawBox(x + (self.GetHeight() + w) * 2, y + (self.GetHeight() + h) * 3)
+        self.DrawBox(x + (self.GetHeight() + w) * 3, y + (self.GetHeight() + h) * 3)
+        self.DrawBox(x + (self.GetHeight() + w) * 4, y + (self.GetHeight() + h) * 3)
+        self.DrawBox(x + (self.GetHeight() + w) * 5, y + (self.GetHeight() + h) * 3)
+        self.DrawBox(x + (self.GetHeight() + w) * 6, y + (self.GetHeight() + h) * 3)
+
+        self.DrawBox(x + (self.GetHeight() + w) * -1, y + (self.GetHeight() + h) * 4)
+        self.DrawBox(x + (self.GetHeight() + w) * 0, y + (self.GetHeight() + h) * 4)
+        self.DrawBox(x + (self.GetHeight() + w) * 1, y + (self.GetHeight() + h) * 4)
+        self.DrawBox(x + (self.GetHeight() + w) * 2, y + (self.GetHeight() + h) * 4)
+        self.DrawBox(x + (self.GetHeight() + w) * 3, y + (self.GetHeight() + h) * 4)
+        self.DrawBox(x + (self.GetHeight() + w) * 4, y + (self.GetHeight() + h) * 4)
+        self.DrawBox(x + (self.GetHeight() + w) * 5, y + (self.GetHeight() + h) * 4)
 
     def SetPosToCard(self, key, value):
         self.id[key] = value
@@ -35,8 +87,15 @@ class Field():
         for i in cards:
             self.deck.append(i[0])
             self.mainDeck.append(i[0])
-
         numpy.random.shuffle(self.mainDeck)
+
+        opGetDeck = self.myDB.cursor()
+        opGetDeck.execute("SELECT cardId FROM decks WHERE userID = 'yugi' ORDER BY name")
+        opCards = opGetDeck.fetchall()
+        for i in opCards:
+            self.opDeck.append(i[0])
+            self.opMainDeck.append(i[0])
+        numpy.random.shuffle(self.opMainDeck)
 
     def GetHand(self):
         hand = 0
@@ -48,10 +107,31 @@ class Field():
             hand = hand + 1
         return self.hand
 
+    def OpGetHand(self):
+        hand = 0
+        for i in reversed(self.opMainDeck):
+            if hand == 5:
+                break
+            self.opMainDeck.remove(i)
+            self.opHand.append(i)
+            hand = hand + 1
+        return self.opHand
+
     def GetDeckString(self):
         fieldInfo = ''
         n = 0
         for i in self.deck:
+            if n > 0:
+                fieldInfo = fieldInfo + ':' + i
+            else:
+                fieldInfo = fieldInfo + i
+                n = n + 1
+        return fieldInfo
+
+    def OpGetDeckString(self):
+        fieldInfo = ''
+        n = 0
+        for i in self.opDeck:
             if n > 0:
                 fieldInfo = fieldInfo + ':' + i
             else:
@@ -73,3 +153,68 @@ class Field():
 
     def GetHeight(self):
         return math.floor(self.width * self.RATIO)
+
+    def MyLifePoint(self, x, y):
+        l = 20
+        num = 0
+        for n in str(self.lifePoints):
+            if n == '0':
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y), (x + l + num * (l + 10), y), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + 2 * l),(x + l + num * (l + 10), y + 2 * l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y), (x + num * (l + 10), y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + l), (x + num * (l + 10), y + 2 * l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y), (x + num * (l + 10) + l, y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y + l),(x + num * (l + 10) + l, y + 2 * l), 1)
+            elif n == '1':
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y), (x + num * (l + 10) + l, y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y + l),(x + num * (l + 10) + l, y + 2 * l), 1)
+            elif n == '2':
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y), (x + l + num * (l + 10), y), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + l), (x + l + num * (l + 10), y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + 2 * l),(x + l + num * (l + 10), y + 2 * l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + l), (x + num * (l + 10), y + 2 * l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y), (x + num * (l + 10) + l, y + l), 1)
+            elif n == '3':
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y), (x + l + num * (l + 10), y), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + l), (x + l + num * (l + 10), y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + 2 * l), (x + l + num * (l + 10), y + 2 * l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y), (x + num * (l + 10) + l, y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y + l), (x + num * (l + 10) + l, y + 2 * l), 1)
+            elif n == '4':
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + l), (x + l + num * (l + 10), y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y), (x + num * (l + 10), y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y), (x + num * (l + 10) + l, y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y + l), (x + num * (l + 10) + l, y + 2 * l), 1)
+            elif n == '5':
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y), (x + l + num * (l + 10), y), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + l), (x + l + num * (l + 10), y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + 2 * l), (x + l + num * (l + 10), y + 2 * l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y), (x + num * (l + 10), y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y + l), (x + num * (l + 10) + l, y + 2 * l), 1)
+            elif n == '6':
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y), (x + l + num * (l + 10), y), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + l), (x + l + num * (l + 10), y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + 2 * l), (x + l + num * (l + 10), y + 2 * l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y), (x + num * (l + 10), y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + l), (x + num * (l + 10), y + 2 * l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y + l), (x + num * (l + 10) + l, y + 2 * l), 1)
+            elif n == '7':
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y), (x + l + num * (l + 10), y), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y), (x + num * (l + 10) + l, y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y + l), (x + num * (l + 10) + l, y + 2 * l), 1)
+            elif n == '8':
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y), (x + l + num * (l + 10), y), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + l), (x + l + num * (l + 10), y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + 2 * l), (x + l + num * (l + 10), y + 2 * l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y), (x + num * (l + 10), y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + l), (x + num * (l + 10), y + 2 * l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y), (x + num * (l + 10) + l, y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y + l), (x + num * (l + 10) + l, y + 2 * l), 1)
+            elif n == '9':
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y), (x + l + num * (l + 10), y), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + l), (x + l + num * (l + 10), y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y + 2 * l), (x + l + num * (l + 10), y + 2 * l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10), y), (x + num * (l + 10), y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y), (x + num * (l + 10) + l, y + l), 1)
+                pygame.draw.line(self.screen, 'red', (x + num * (l + 10) + l, y + l), (x + num * (l + 10) + l, y + 2 * l), 1)
+            num = num + 1
